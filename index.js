82180238b88370ba1957fd9ee4f635728f660791
Duretitibesso3 <gdureti@gmail.com>
2022-05-18 03:14:49 -0500

    Add index.js file

---
+++ index.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector('#hamburger');
+const close = document.querySelector('#close');
+const navigation = document.querySelector('.nav-ul');
+const textPortfolio = document.querySelector('.nav-text-1');
+const about = document.querySelector('.nav-text-2');
+const contact = document.querySelector('.nav-text-3');
