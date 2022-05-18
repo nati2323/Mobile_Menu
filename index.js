20c5b1b1a38a42f41b126705f1ad9362556daef6
nati2323 <nati2.man.3@gmail.com>
2022-05-18 11:18:25 +0300

    Add Css file with background image

--- index.js
+++ index.js
@@ -4,3 +4,27 @@ const navigation = document.querySelector('.nav-ul');
 const textPortfolio = document.querySelector('.nav-text-1');
 const about = document.querySelector('.nav-text-2');
 const contact = document.querySelector('.nav-text-3');
+
+// hamburger menu
+hamburger.addEventListener('click', (e) => {
+    e.preventDefault();
+  
+    navigation.classList.toggle('show');
+    hamburger.classList.add('hide');
+    close.classList.remove('hide');
+    textPortfolio.classList.add('menu-text');
+    about.classList.add('menu-text');
+    contact.classList.add('menu-text');
+  });
+  
+  close.addEventListener('click', () => {
+    hamburger.classList.remove('hide');
+    navigation.classList.remove('show');
+  });
+  
+  textPortfolio.addEventListener('click', (e) => {
+    e.stopPropagation();
+  
+    navigation.classList.remove('show');
+    hamburger.classList.remove('hide');
+  });
